4c55be7af68c6e1e1530b2e1903a4f6c16fc0910
mateus-mendess <mendes.profissional12@gmail.com>
2026-04-24 11:05:58 -0300

    feat: Initial commit.

---
+++ src/main/java/com/br/turbrasil/TurbrasilApplication.java
@@ -0,0 +1,13 @@
+package com.br.turbrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbrasilApplication.class, args);
+	}
+
+}
